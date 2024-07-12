ae664a9ef078d4fbaed1cfc3bd8392a3923b54b4
mukhlisbara <74233535+mukhlisbara@users.noreply.github.com>
2024-07-12 22:56:58 +0700

    feat: add-primary-button (#1)

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+const ButtonPrimary = () => {
+    return (
+        <>
+            <Button variant='primary'>
+                Primary button
+            </Button>
+        </>
+    );
+};
+
+export default ButtonPrimary;

ef466c1e6ac363865a3ffc6f1d25c131138f7961
mukhlisbara <74233535+mukhlisbara@users.noreply.github.com>
2024-07-15 23:48:38 +0700

    fixing header-footer (#7)
    Co-authored-by: Hafiidh Luqmanul Hakim <hafidluqmanulhakim@gmail.com>

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React, { Children } from 'react'
 import Header from './Header'
 import { Button } from 'react-bootstrap'
 import Back from './Back'
 import SumberRekening from '../components/SumberRekening'
 import Footer from './Footer'
-import { Outlet } from "react-router-dom";
 
-
-export default function Layout() {
+export default function Layout({ children }) {
     return (
         <div className="App">
             <Header />
             <main>
-                <Outlet />
+                {children}
             </main>
             <div className="card footer-card" >
                 <Footer />

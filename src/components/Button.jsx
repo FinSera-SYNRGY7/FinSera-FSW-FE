f4b53f603471e029460b678f4f6f6bb120399afb
dzikrikarh <67620766+dzikrikarh@users.noreply.github.com>
2024-07-17 19:26:18 +0700

    feat: add login page (#10)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-const ButtonPrimary = () => {
+const ButtonPrimary = ({ children, style, ...props }) => {
     return (
         <>
             <Button className='button-beranda' variant='$0066AE'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+import App from './App.jsx'
+import Login from './layout/Login.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <BrowserRouter>
       <App />
+      <Login />
     </BrowserRouter>
   </React.StrictMode>,
 )

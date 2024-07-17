a1f867ee35b63cc5f2e191c996b9128a21a33e82
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-07-17 16:20:00 +0700

    feat: change variant button icon

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -2,23 +2,67 @@ import React from "react";
 
 interface ButtonIconProps {
   label: string,
-  color: string,
-  icon: string,
   onClick: () => void,
-  variant: "solid" | "outline"
+  variant: "btnDownload2nd" | "btnDownload1st" | "btnAdd" | "btnShare" | "btnBack"
 }
 
-const ButtonIcon:React.FC<ButtonIconProps> = ({label, color, icon, onClick, variant}) => {
+const ButtonIcon: React.FC<ButtonIconProps> = ({label, onClick, variant}) => {
 
   const ButtonIconStyle: { [key: string]: React.CSSProperties }  = {
-    button: {
-      padding: "14px 32px",
-      backgroundColor: variant === "solid" ? color : "white",
-      color: variant === "solid" ? "white" : color,
+    btnDownload2nd: {
+      padding: "12px 41px",
+      backgroundColor: "white",
+      color: "#0066AE",
       display: "flex",
       flexDirection: "row",
-      alignItems: "flex-end",
-      border: variant === "outline" ? `2px solid ${color}` : "none",
+      alignItems: "center",
+      border: "2px solid #0066AE",
+      borderRadius: "16px",
+    },
+    btnDownload1st: {
+      padding: "12px 0",
+      backgroundColor: "#0066AE",
+      color: "white",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      border: "none",
+      borderRadius: "16px",
+      width: "27%"
+    },
+    btnShare: {
+      padding: "12px 0",
+      backgroundColor: "#0066AE",
+      color: "white",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      border: "none",
+      borderRadius: "16px",
+      width: "27%"
+    },
+    btnBack: {
+      padding: "12px 0 12px 25px",
+      backgroundColor: "#0066AE",
+      color: "white",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      border: "none",
+      borderRadius: "16px",
+      width: "888px"
+    },
+    btnAdd: {
+      padding: "12px 22px",
+      backgroundColor: "white",
+      color: "#0066AE",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      border: "2px solid #0066AE",
       borderRadius: "16px",
     },
     icon: {
@@ -28,13 +72,29 @@ const ButtonIcon:React.FC<ButtonIconProps> = ({label, color, icon, onClick, vari
       margin: "0 0 0 20px",
       fontWeight: 700,
       fontSize: "16px",
+      fontFamily:"calibri"
     }
   }
 
+  const getIconClass = (variant: string) => {
+    switch (variant) {
+      case "btnDownload2nd":
+      case "btnDownload1st":
+        return "fa fa-download";
+      case "btnAdd":
+        return "fa fa-plus";
+      case "btnShare":
+        return "fa fa-share-alt";
+      case "btnBack":
+        return "fa fa-arrow-left";
+      default:
+        return "";
+    }
+  }
 
   return (
-    <button className="btn" style={ButtonIconStyle.button} onClick={onClick}>
-      <i className={icon} style={ButtonIconStyle.icon}></i>
+    <button className="btn" style={ButtonIconStyle[variant]} onClick={onClick}>
+      <i className={getIconClass(variant)} style={ButtonIconStyle.icon}></i>
       <p style={ButtonIconStyle.text}>{label}</p>
     </button>
   )

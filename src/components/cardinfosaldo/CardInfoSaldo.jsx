5d5b23dcff9792c29e5ba8f26b43708159f2cc24
mukhlisbara <mukhlisbara12@gmail.com>
2024-07-21 10:30:51 +0700

    refactor:try fixing header footer

--- src/components/cardinfosaldo/CardInfoSaldo.jsx
+++ src/components/cardinfosaldo/CardInfoSaldo.jsx
@@ -1,19 +1,19 @@
 import Card from 'react-bootstrap/Card';
-// import CardInfoSaldoStyle from './cardinfosaldo/CardInfoSaldoStyle.css'
-import styles from "@/cardinfosaldo/CardInfoSaldoStyle.css";
+// import styles from "@/cardinfosaldo/CardInfoSaldoStyle.css";
+import "./CardInfoSaldoStyle.css"
 
 // import LogoBcaHalf from '../assets/logo/logo-bca-half.png';
 
 function CardInfoSaldo({ profile, saldo, norek }) {
     return (
-        <div className="d-flex flex-column align-items-start" style={styles.Container}>
-            <h2 style={styles.profile}>{profile}</h2>
-            <div className="containerCard d-flex flex-row" style={styles.containecard}>
+        <div className="d-flex flex-column align-items-start Container" >
+            <h2>{profile}</h2>
+            <div className="containerCard d-flex flex-row containecard">
                 <div className="content1 d-flex flex-column align-items-start">
-                    <h2 style={styles.norek}>{norek}</h2>
+                    <h2>{norek}</h2>
                 </div>
                 <div className="content2 d-flex flex-column align-items-end">
-                    <h2 style={styles.saldo}>{saldo}</h2>
+                    <h2>{saldo}</h2>
                 </div>
             </div>
         </div>

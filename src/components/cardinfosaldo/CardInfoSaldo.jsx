8ade3fc8ac0aefffb60dd6892f1a7fdca42f8902
mukhlisbara <mukhlisbara12@gmail.com>
2024-07-21 20:12:01 +0700

    fix:page info saldo

--- src/components/cardinfosaldo/CardInfoSaldo.jsx
+++ src/components/cardinfosaldo/CardInfoSaldo.jsx
@@ -1,6 +1,8 @@
 import Card from 'react-bootstrap/Card';
 import './CardInfoSaldoStyle.css'
 import gambar1 from "@/assets/logo/logo-bca-half.png"
+import mastercard from "@/assets/logo/mastercard.png"
+
 
 // import LogoBcaHalf from '../assets/logo/logo-bca-half.png';
 
@@ -22,19 +24,20 @@ function CardInfoSaldo({ profile, saldo, norek }) {
         <div className="w-100 d-flex flex-column bg-saldo text-start text-white card-saldo">
             <div className="row">
                 <div className="col-10">
-                    <div className="px-5 py-3">
+                    <div className="px-5 py-4">
                         <h3 className="text-user">Hi, {profile}</h3>
                     </div>
-                    <div className="px-5 py-3">
+                    <div className="px-5 py-1">
                         <h6 className="text-saldo">Saldo Aktif</h6>
                         <h2 className="text-jumlah-saldo">Rp. {saldo}</h2>
                     </div>
-                    <div className="px-5">
+                    <div className="px-5 d-flex align-items-center">
+                        <img src={mastercard} alt="" />
                         <h2 className="text-no-rek">{norek}</h2>
                     </div>
                 </div>
                 <div className="col-2">
-                    <img src={gambar1} alt="" className="img-fluid" />
+                    <img src={gambar1} alt="" className="img-fluid bg-logo-bca" />
                 </div>
             </div>
         </div>
--- src/pages/InfoSaldo.jsx
+++ src/pages/InfoSaldo.jsx
@@ -4,15 +4,35 @@ import Layout from '@/layout/Layout'
 import SumberRekening from '@/components/sumberrekening/SumberRekening'
 import CardInfoSaldo from '@/components/cardinfosaldo/CardInfoSaldo'
 import ButtonIcon from '@/components/ButtonIcon'
-import BtnDropdown from "@/components/dropdown/Dropdwon";
+import DropdownSumberRekening from "@/components/dropdownSumberRekening/Dropdown";
+
 
 
 export default function InfoSaldo() {
+
+    const [selectedOption, setSelectedOption] = useState("");
+    const [emptyData, setEmptyData] = useState(false);
+
+    const handleOptionSelect = (value) => {
+        setSelectedOption(value);
+        console.log("Selected option:", value);
+    };
+
+    const handleEmptyData = () => {
+        setEmptyData(true);
+    };
+
+    const options = [
+        { value: "action1", label: "Action 1" },
+        { value: "action2", label: "Action 2" },
+        { value: "action3", label: "Action 3" },
+    ];
+
     return (
         <Layout>
             <div className="d-flex flex-column containerInfoSaldo">
                 <h1>Informasi Saldo</h1>
-                <div className='d-flex flex-row'>
+                {/* <div className='d-flex flex-row'>
                     <ButtonIcon
                         label="Beranda"
                         onClick={() => console.log("Outline Add Button Clicked")}
@@ -21,6 +41,21 @@ export default function InfoSaldo() {
                     <SumberRekening
                         norek="1234 567 897 890"
                         className="dropdown-sumberrekening"
+                    />
+                </div> */}
+
+                <div className="d-flex flex-row btnSection">
+                    <ButtonIcon
+                        label="Beranda"
+                        onClick={() => console.log("Outline Add Button Clicked")}
+                        variant="btnBack"
+                    />
+                    <DropdownSumberRekening
+                        options={options}
+                        onOptionSelect={handleOptionSelect}
+                        title="Sumber Rekening"
+                        subtitle="1234 5678 8765 99"
+                        className="dropdownSumberRekening"
                     />
                 </div>
 

e3d5d05b8f098fadfc31b9e8d1a14a93fa42165e
dzikrikarh <67620766+dzikrikarh@users.noreply.github.com>
2024-07-26 15:40:27 +0700

    feat: update responsive and aria label to login and relog pages (#23)

--- src/components/PinInput.jsx
+++ src/components/PinInput.jsx
@@ -1,7 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 export const PinInput = ({ style, ...otherProps }) => {
   const inputRefs = useRef([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const handleChange = (e, index) => {
     if (e.target.value.length >= 1 && index < inputRefs.current.length - 1) {
@@ -15,14 +22,34 @@ export const PinInput = ({ style, ...otherProps }) => {
     }
   };
 
-  const inputStyle = {
-    width: '45px',
-    height: '45px',
-    borderRadius: '10px',
-    border: '2px solid #AAA',
-    margin: '0 15px',
-    textAlign: 'center',
-    fontSize: '1.2em'
+  const getInputStyle = () => {
+    let baseStyle = {
+      width: '45px',
+      height: '45px',
+      borderRadius: '10px',
+      border: '2px solid #AAA',
+      margin: '0 15px',
+      textAlign: 'center',
+      fontSize: '1.2em'
+    };
+
+    if (windowWidth <= 600) {
+      return {
+        ...baseStyle,
+        width: '45px',
+        height: '45px',
+        margin: '0 10px',
+      };
+    } else if (windowWidth <= 768) {
+      return {
+        ...baseStyle,
+        width: '50px',
+        height: '50px',
+        margin: '0 13px',
+      };
+    }
+
+    return baseStyle;
   };
 
   const inputsStyle = {
@@ -38,7 +65,7 @@ export const PinInput = ({ style, ...otherProps }) => {
         <input
           key={i}
           type="text"
-          style={inputStyle}
+          style={getInputStyle()}
           maxLength="1"
           inputMode="numeric"
           ref={el => inputRefs.current[i] = el}

c79bbece7301eaf822285c0b7c88ac7faa555bf4
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-07-28 14:41:52 +0700

    refactor: merge button icon to button alter

--- src/components/ButtonAlt.jsx
+++ src/components/ButtonAlt.jsx
@@ -1,54 +1,43 @@
 import React from "react";
+import styles from "../assets/css/ButtonAlt.module.css"
 
 
 
 const ButtonAlt = ({label, onClick, variant}) => {
 
-  const ButtonAltStyle = {
-    containerButton : {
-      width: "100%",
-    },
-    btnPrimary: {
-      backgroundColor: "#0066AE",
-      color: "white",
-      width: "80%",
-      padding: "12px 0",
-      borderRadius: "16px",
-    },
-    btnSecondary: {
-      padding: "12px 0",
-      backgroundColor: "white",
-      color: "#0066AE",
-      borderRadius: "16px",
-      border: "2px solid #0066AE",
-      width: "80%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    btnSecondaryIcon: {
-      padding: "12px 0",
-      backgroundColor: "white",
-      color: "#0066AE",
-      borderRadius: "16px",
-      border: "2px solid #0066AE",
-      width: "80%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    text: {
-      margin: "0",
-      fontWeight: 700,
-      fontSize: "16px",
-      fontFamily:"calibri"
+  return (
+    <div className={styles.containerButtonAlt}>
+      <button className={`${styles[variant]}`} onClick={onClick} aria-label={label}>
+      <p>{label}</p>
+    </button>
+    </div>
+  )
+}
+
+const ButtonIcon = ({ label, onClick, variant }) => {
+  const containerClassButton = variant === "btnAdd" ? styles.containerButton + " " + styles.btnAdd : styles.containerButton + " " + styles.default;
+
+  const getIconClass = (variant) => {
+    switch (variant) {
+      case "btnDownload2nd":
+      case "btnDownload1st":
+        return "fa fa-download";
+      case "btnAdd":
+        return "fa fa-plus";
+      case "btnShare":
+        return "fa fa-share-alt";
+      case "btnBack":
+        return "fa fa-arrow-left";
+      default:
+        return "";
     }
   }
 
   return (
-    <div style={ButtonAltStyle.containerButton}>
-      <button className="btn" style={ButtonAltStyle[variant]} onClick={onClick}>
-      <p style={ButtonAltStyle.text}>{label}</p>
+    <div className={`d-flex ${containerClassButton}`}>
+      <button className={`d-flex flex-row justify-content-center align-items-center ${styles[variant]}`} onClick={onClick} aria-label={label}>
+      <i className={`${getIconClass(variant)} ${styles.icon}`} aria-hidden="true"></i>
+      <p className={styles.textLabel}>{label}</p>
     </button>
     </div>
   )
@@ -57,4 +46,5 @@ const ButtonAlt = ({label, onClick, variant}) => {
 
 export {
   ButtonAlt,
+  ButtonIcon,
 };

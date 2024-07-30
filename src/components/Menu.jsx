ce2c5e6770be3a604dfe039ae2c3ca5e28bfd04f
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-07-30 14:46:23 +0700

    feat: fixing layout homepage

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,7 +7,6 @@ const ServiceMenu = ({ label, navigation, icon }) => {
     <div className={`${styles.containerServiceMenu} d-flex flex-column justify-content-center align-items-center`}>
         <div
           className={`${styles.logoMenu} d-flex flex-column justify-content-center align-items-center mb-3`}
-          aria-hidden="true"
         >
           <img src={icon} alt={label} />
         </div>

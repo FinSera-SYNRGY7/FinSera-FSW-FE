48b3df292dd4af8af52c5f7c444cdbc7c0b7c6fb
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2024-08-09 21:37:45 +0800

    feat(relog): redirect to relog page if pin already set

--- src/features/auth/useRefreshToken.js
+++ src/features/auth/useRefreshToken.js
@@ -1,11 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import { httpServer } from '@/lib/server'
+import axios from 'axios'
 
 
 export const useRefreshToken = ({ onSuccess, onError }) => {
   const fetchRefreshToken = async (requestToken) => {
+    console.log(requestToken)
     // const response = await axios.post(`${import.meta.env.VITE_AXIOS_BASE_URL}/api/v1/auth/user/login`, requestDataUsers);
-    const response = await httpServer.post(`/api/v1/auth/user/refresh-token`, requestToken);
+    const response = await axios.post(`https://finsera-api.site/api/v1/auth/user/refresh-token`, requestToken);
     
     return response.data;
   };

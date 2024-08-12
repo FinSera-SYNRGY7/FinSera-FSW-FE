4ef38969f2a168ba7fdebdf081248ae56d3853fe
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-08-12 17:36:32 +0700

    feat: change card vertical

--- src/pages/InterbankTfError.jsx
+++ src/pages/InterbankTfError.jsx
@@ -2,32 +2,56 @@ import React from "react";
 import Layout from "@/layout/Layout";
 import Button from "@/components/Button/index";
 import Error from "@/assets/img/Error.svg";
+import { Link } from "react-router-dom";
 
 const InterbankTfError = () => {
   return (
     <Layout className={"haveStyle"}>
       <div className="d-flex align-items-baseline pt-5">
-        <Button
-          className="d-sm-none p-0"
-          type="button"
-          aria-label="kembali ke halaman sebelumnya"
+      <Link
+          to="/home"
+          className="m-0"
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+          }}
+          role="button"
+          aria-label="kembali ke halaman home"
         >
-          <i className="fa fa-arrow-left" />
-        </Button>
+          <Button
+            className="d-sm-none p-0"
+            type="button"
+            aria-label="kembali ke halaman sebelumnya"
+          >
+            <i className="fa fa-arrow-left" />
+          </Button>
+        </Link>
         <h1 className="fw-bold col-12 text-center text-sm-start">
           <span role="label" aria-label="Transfer Antar Bank">
             Transfer Antar Bank
           </span>
         </h1>
       </div>
-      <Button
-        className={"d-none d-sm-block col-sm-12 base-color text-sm-start mb-5"}
-        type="button"
+      <Link
+        to="/home"
+        style={{
+          textDecoration: "none",
+          color: "inherit",
+        }}
         aria-label="kembali ke halaman sebelumnya"
+        role="button"
       >
-        <i className="fa fa-arrow-left" />
-        <span className="ms-20">Back</span>
-      </Button>
+        <Button
+          className={
+            "d-none d-sm-block col-sm-12 base-color text-sm-start mb-5 shadow-hover"
+          }
+          type="button"
+          aria-label="kembali ke halaman sebelumnya"
+        >
+          <i className="fa fa-arrow-left" />
+          <span className="ms-20">Back</span>
+        </Button>
+      </Link>
       <div className="container text-center p-5">
         <img className="w-100" src={Error} alt="Error" />
         <span role="label" aria-label="Oops! Sepertinya ada masalah">
--- src/pages/InterbankTfSuccess.jsx
+++ src/pages/InterbankTfSuccess.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Layout from "@/layout/Layout";
-import { CardVertical } from "@/components/Card/index";
 import { useLocation, Link } from "react-router-dom";
-
+import { CardVerticalAlt } from "../components/Card";
+import { CardVertical } from "../components/Card/index";
 
 const InterbankTfSuccess = () => {
   const { state } = useLocation();
@@ -10,39 +10,43 @@ const InterbankTfSuccess = () => {
   console.log(state);
 
   return (
-    <Layout type={"necktie"} className={"haveStyle"}>
-      <div style={{ marginTop: "-20vh" }}></div>
-      <CardVertical
-        className={"shadow p-0 border-0 mb-5 px-1 px-md-5 success"}
-        // data={{
-        //   transaction_date: state.transaction_date,
-        //   transaction_num: state.transaction_num,
-        //   accountnum_recipient: state.accountnum_recipient,
-        //   name_recipient: state.name_recipient,
-        //   nominal: state.nominal,
-        //   note: state.note,
-        // }}
-      >
-        <div className="d-flex justify-content-end mb-3 mb-sm-5">
-          <Link
-            to="/home"
-            className="m-0"
-            style={{
-              textDecoration: "none",
-              color: "inherit",
-            }}
-            role="button"
-            aria-label="kembali ke halaman home"
-          >
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-            ></button>
-          </Link>
-        </div>
-      </CardVertical>
-    </Layout>
-  )
+    <div>
+      <Layout type={"necktie"} className={"haveStyle"}>
+        <div style={{ marginTop: "-20vh" }}></div>
+        <CardVerticalAlt
+          className={"shadow p-0 border-0 mb-5 px-1 px-md-5 success"}
+          data={{
+            transaction_date: "state.transaction_date",
+            transaction_num: "state.transaction_num",
+            accountnum_recipient: "state.accountnum_recipient",
+            name_recipient: "state.name_recipient",
+            type_transaksi: "Transfer Antar Bank",
+            nominal: "state.nominal",
+            nominal_admin: "state.nominal_admin",
+            note: "state.note",
+          }}
+        >
+          <div className="d-flex justify-content-end mb-3 mb-sm-5">
+            <Link
+              to="/home"
+              className="m-0"
+              style={{
+                textDecoration: "none",
+                color: "inherit",
+              }}
+              role="button"
+              aria-label="kembali ke halaman home"
+            >
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+              ></button>
+            </Link>
+          </div>
+        </CardVerticalAlt>
+      </Layout>
+    </div>
+  );
 };
 export default InterbankTfSuccess;
